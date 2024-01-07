2c6d29d2f9e4968ee56e89a9b62bb36e00b691f5
3khaled3 <ff.tark123@gmail.com>
2024-01-07 08:27:42 +0200

    done

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -15,38 +15,39 @@ class MyApp extends StatelessWidget {
       ),
       supportedLocales: [
         const Locale('ar'),
-        // const Locale('en'),
-        // const Locale('es'),
-        // const Locale('de'),
-        // const Locale('fr'),
-        // const Locale('el'),
-        // const Locale('et'),
-        // const Locale('nb'),
-        // const Locale('nn'),
-        // const Locale('pl'),
-        // const Locale('pt'),
-        // const Locale('ru'),
-        // const Locale('hi'),
-        // const Locale('ne'),
-        // const Locale('uk'),
-        // const Locale('hr'),
-        // const Locale('tr'),
-        // const Locale('lv'),
-        // const Locale('lt'),
-        // const Locale('ku'),
-        // const Locale('nl'),
-        // const Locale('it'),
-        // const Locale('ko'),
-        // const Locale('ja'),
-        // const Locale('id'),
-        // const Locale('cs'),
-        // const Locale.fromSubtags(
-        //     languageCode: 'zh',
-        //     scriptCode: 'Hans'), // Generic Simplified Chinese 'zh_Hans'
-        // const Locale.fromSubtags(
-        //     languageCode: 'zh',
-        //     scriptCode: 'Hant'), // Generic traditional Chinese 'zh_Hant'
+        const Locale('en'),
+        const Locale('es'),
+        const Locale('de'),
+        const Locale('fr'),
+        const Locale('el'),
+        const Locale('et'),
+        const Locale('nb'),
+        const Locale('nn'),
+        const Locale('pl'),
+        const Locale('pt'),
+        const Locale('ru'),
+        const Locale('hi'),
+        const Locale('ne'),
+        const Locale('uk'),
+        const Locale('hr'),
+        const Locale('tr'),
+        const Locale('lv'),
+        const Locale('lt'),
+        const Locale('ku'),
+        const Locale('nl'),
+        const Locale('it'),
+        const Locale('ko'),
+        const Locale('ja'),
+        const Locale('id'),
+        const Locale('cs'),
+        const Locale.fromSubtags(
+            languageCode: 'zh',
+            scriptCode: 'Hans'), // Generic Simplified Chinese 'zh_Hans'
+        const Locale.fromSubtags(
+            languageCode: 'zh',
+            scriptCode: 'Hant'), // Generic traditional Chinese 'zh_Hant'
       ],
+      locale: Locale('ar'),
       localizationsDelegates: [
         CountryLocalizations.delegate,
         GlobalMaterialLocalizations.delegate,
@@ -63,6 +64,9 @@ class HomePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    CountryLocalizations? countryLocalizations =
+        CountryLocalizations.of(context);
+
     return Directionality(
       textDirection: TextDirection.rtl,
       child: Scaffold(
